ca50b90b858318dbecabf8c4bb25fbe0fd430d96
Andres <andresjml@gmail.com>
2021-06-03 15:46:36 -0400

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
+
 function App() {
+  const [data, setdata]=useState([])
+  const [wallet, setWallet] = useState(100);
+  
+  useEffect(() =>{
+    fetch(API)
+      .then(r => r.json())
+      .then((sushis) => {
+        const updatedSushis = sushis.map((sushi) => {
+          return { ...sushi, eaten: false };
+        });
+        setdata(updatedSushis);
+      });
+  }, []);
+
+  function handleEatSushi(eatenSushi) {
+    if (wallet >= eatenSushi.price) {
+      const updatedSushis = data.map((sushi) => {
+        if (sushi.id === eatenSushi.id) return { ...sushi, eaten: true };
+        return sushi;
+      });
+
+      setdata(updatedSushis);
+      setWallet((wallet) => wallet - eatenSushi.price);
+    } else {
+      alert("Need more 💸");
+    }
+  }
+
+  const eatenSushis = data.filter((sushi) => sushi.eaten);
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis={data} onEatSushi={handleEatSushi}/>
+      <Table  wallet={wallet} plates={eatenSushis}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+
+
+function SushiContainer({sushis, onEatSushi}) {
+  const [sushiIndex, setSushiIndex] = useState(0);
+
+
+  
+
+  const sushiComponents = sushis
+    .slice(sushiIndex, sushiIndex + 4)
+    .map((sushi) => (
+      <Sushi key={sushi.id} sushi={sushi} onEatSushi={onEatSushi}/>
+    ));
+
+    function handleClickMore() {
+      setSushiIndex((sushiIndex) => (sushiIndex + 4) );
+    }
+   
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiComponents}
+      <MoreButton clickMore={handleClickMore}/>
     </div>
   );
 }
